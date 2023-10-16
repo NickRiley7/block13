8b03dae05e91ae40b7fac7adc74c08284c1cc3fd
NickRiley7 <nick_riley@LAPTOP-J8V35VGC>
2023-10-16 16:01:59 -0400

    starter code

---
+++ code.js
@@ -0,0 +1,33 @@
+function convertToCelsius(fahren) {}
+
+function createMessage(fahren, celc) {}
+
+function rand(limit) {}
+
+// I have no idea what im even supposed to do, this makes no sense to me //
+
+// -------------------- DONT NOT CHANGE THE CODE BELOW ---------------------- //
+
+let fahren = prompt(
+    "enter a number, we will convert that number from fahrenheit to celcius"
+);
+let celc = convertToCelcius(fahren);
+let ooutput = createMessage(fahren, celc);
+console.log(output);
+
+fahren = prompt(
+    "lets try that again. Enter a number, we will convert that number from fahrenheit to celsius"
+);
+celc = convertToCelsius(fahren);
+output = createMessage(fahren, celc);
+console.log(output);
+
+fahren = rand(110);
+celc = convertToCelsius(fahren);
+output = createMessage(fahren, celc);
+console.log(output);
+
+fahren = rand(110);
+celc = convertToCelsius(fahren);
+output = createMessage(fahren, celc);
+console.log(output);
